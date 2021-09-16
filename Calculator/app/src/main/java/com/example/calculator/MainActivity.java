1320b01e2eb850abe504ffe56f19916f786ed680
hanum <rntls123@naver.com>
2021-09-16 18:41:38 +0900

    09.16 Lecture - calculator

--- Calculator/app/src/main/java/com/example/calculator/MainActivity.java
+++ Calculator/app/src/main/java/com/example/calculator/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.calculator;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onButtonClick(View view){
+        EditText num1 = (EditText) findViewById(R.id.input1);
+        EditText num2 = (EditText) findViewById(R.id.input2);
+        String str1 = num1.getText().toString();
+        String str2 = num2.getText().toString();
+        TextView resultOper = (TextView) findViewById(R.id.labelResult);
+        int result = 0;
+        switch (view.getId()) {
+            case R.id.buttonAdd:
+                result = Integer.parseInt(str1) + Integer.parseInt(str2);
+                break;
+            case R.id.buttonSubtract:
+                result = Integer.parseInt(str1) - Integer.parseInt(str2);
+                break;
+            case R.id.buttonMultiply:
+                result = Integer.parseInt(str1) * Integer.parseInt(str2);
+                break;
+            case R.id.buttonDivide:
+                result = Integer.parseInt(str1) / Integer.parseInt(str2);
+                break;
+        }
+        resultOper.setText("Result: " + result);
     }
 }
